ba58856e2fccc580949e6a1aecd12552c540a39f
Martin Gorren <Martin.Gorren@outlook.com>
2026-04-10 18:06:05 +0200

    Improved architecture, the data is fetched from a different file.
    Adjusted the fetching logic to handle various type of inputs

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,11 +1,12 @@
 import requests
+import json
 # API Ninjas key:
 API_KEY = "NGvVjduynuMZhmbQ07E7yQhMLvLZFRXtOB3E3uEp"
 
 BASE_URL = "https://api.api-ninjas.com/v1/"
 
 
-def fetches_data(animal):
+def fetches_data_from_get(animal):
     request_URL = f"{BASE_URL}animals?name={animal}"
     headers = {"X-Api-Key": API_KEY}
     print("Making the following request", request_URL)
@@ -16,10 +17,12 @@ def fetches_data(animal):
     animals = animals.json()
     return animals
 
-def fetch_data_from_file(file_path):
+def fetches_data_from_file(file_path, animal:str):
+    if animal.lower() != "fox":
+        return ""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-def fetch_data(animal):
-    return fetches_data(animal)
+def fetch_data(animal:str):
+    return fetches_data_from_file("animals_data.json", animal)
 
